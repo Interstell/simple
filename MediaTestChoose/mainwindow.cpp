1ee8c5b8218917ef82a76660b6ad581b503f93c7
Danshcherbina <danshcherbina@gmail.com>
2016-05-05 23:42:25 +0300

    Media test
    Now you can choose  *.mp3 file with explorer

---
+++ MediaTestChoose/mainwindow.cpp
@@ -0,0 +1,46 @@
+#include <QMediaPlayer>
+#include <QFileDialog>
+#include <QDir>
+#include <QUrl>
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+QMediaPlayer *player = new QMediaPlayer;
+int i=0;
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_Start_clicked()
+{
+    QString str1=QFileDialog::getOpenFileName(this, tr("Open File"),"/home",
+                                                    tr("Music (*.mp3)"));
+    //QString str1=ui->lineEdit->text();
+    player->setMedia(QUrl::fromLocalFile(QDir::toNativeSeparators(str1)));
+    player->setVolume(ui->spinBox->value());
+    player->play();
+}
+void MainWindow::on_Stop_clicked()
+{
+    player->pause();
+}
+
+
+void MainWindow::on_Play_clicked()
+{
+    player->play();
+}
+
+void MainWindow::on_spinBox_valueChanged(int arg1)
+{
+    player->setVolume(ui->spinBox->value());
+}
