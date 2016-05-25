e81b0518d434498f45b530e659041f002cbfb41b
NadineIsaeva <nadineisabsent@gmail.com>
2016-05-25 09:13:11 +0300

    piu
    piu

--- Opencv_Test2/mainwindow.cpp
+++ Opencv_Test2/mainwindow.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "cjson.h"
@@ -5,6 +6,25 @@
 #include <QRegion>
 #include <Windows.h>
 
+const char* IPPORT = "192.168.56.101:5000";
+#define NEUTRAL_CORRECTION_PERCENT 0.85
+
+struct EMOTION{
+    char type[20];
+    double value;
+    enum emoEnum type_e;
+};
+
+enum emoEnum{
+    ANGER = 1,
+    CONTEMPT,
+    DISGUST,
+    FEAR,
+    HAPPINESS,
+    NEUTRAL,
+    SADNESS,
+    SURPRISE
+};
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -22,7 +42,11 @@ void MainWindow::on_startBtn_clicked()
 {
     using namespace cv;
     using namespace std;
+    ui->emotionBtn->setEnabled(true);
+    player = new QMediaPlayer;
     VideoCapture cap;
+    cap.set(CV_CAP_PROP_FRAME_WIDTH,1280);
+    cap.set(CV_CAP_PROP_FRAME_HEIGHT,960);
     cap.open(0);
     QPixmap qimg;
 
@@ -44,7 +68,7 @@ void MainWindow::on_startBtn_clicked()
 
     // Vector of returned faces
     vector<Rect> found;
-
+    GetLocalTime(&timeForEmo);
     while(1){
         //Mat img;
         cap >> MainWindow::img;
@@ -86,6 +110,14 @@ void MainWindow::on_startBtn_clicked()
             destroyWindow("EmotionAnalyzer");
             break; //exit
         }
+
+        SYSTEMTIME check;
+        GetLocalTime(&check);
+        long diff = check.wHour*60*60*1000 + check.wMinute*60*1000 + check.wSecond*1000 + check.wMilliseconds - (timeForEmo.wHour*60*60*1000 + timeForEmo.wMinute*60*1000 + timeForEmo.wSecond*1000 + timeForEmo.wMilliseconds);
+        if (diff > EMOWAITTIME){
+            ui->emotionBtn->setEnabled(true);
+            ui->emotionBtn->click();
+        }
         img.release();
     }
 
@@ -95,25 +127,39 @@ void MainWindow::on_startBtn_clicked()
 void MainWindow::on_stopBtn_clicked()
 {
     MainWindow::toCapture = false;
+    ui->emotionBtn->setEnabled(false);
+    ui->label->setVisible(false);
+    delete player;
+    player = NULL;
+    ui->musicTrackNameLabel->setText("");
 }
+
+
+int compareEmos(const void* a, const void * b){
+    double dif = ((struct EMOTION*)b)->value - ((struct EMOTION*)a)-> value;
+    if (dif > 0) return 1;
+    else if (dif < 0) return -1;
+    else return 0;
+}
+
 
 double* MainWindow::emotions_Analyze(char* link){
     char cmd[600];
     char exportFileName[400];
     sprintf(exportFileName, "emotions.json");
     sprintf(cmd, "curl.exe -o %s -XPOST https://api.projectoxford.ai/emotion/v1.0/recognize?subscription-key=349f1c7ff1e642498d333cb2fe5fab25 -k --data-binary @%s -H \"Content-Type:application/octet-stream\"", exportFileName, link);
-    //system(cmd);// WARNING! NO UPDATE IF COMMENTED
     WinExec(cmd,SW_HIDE);
-    //system("cls");
 
     FILE* fJSON = fopen(exportFileName, "r");
 
     char jsonContent[1000];
     fread(jsonContent,sizeof(char),1000,fJSON);
     cJSON* root = cJSON_Parse(jsonContent);
-
-    if (cJSON_GetArraySize(root) == 0){//no face detected
-
+    int size = cJSON_GetArraySize(root);
+    if (size == 0){//no face detected
+        return NULL;
+    }
+    if (cJSON_GetObjectItem(root, "error") != NULL || cJSON_GetObjectItem(root, "message") != NULL ){
         return NULL;
     }
 
@@ -158,65 +204,285 @@ double* MainWindow::emotions_Analyze(char* link){
     return emotionsArray;
 }
 
+char* MainWindow::fromDecToHex(int Saturation, int R, int G, int B){
+    static char result[10];
+    char SHEX[5];
+    char RHEX[5];
+    char GHEX[5];
+    char BHEX[5];
+    _itoa(Saturation, SHEX, 16);
+    _itoa(R, RHEX, 16);
+    _itoa(G, GHEX, 16);
+    _itoa(B, BHEX, 16);
+    sprintf(result, "%02s%02s%02s%02s", SHEX, RHEX, GHEX, BHEX);
+    return result;
+}
+
+void MainWindow::sendColor(int Saturation, int R, int G, int B){
+    char cmd[500];
+    sprintf(cmd, "curl2 -XPOST \"%s\" -d \"cmd:gatttool -b 80:BA:4B:11:AC:E6 --char-write -a 0x0017 -n %s0400ff00\"",IPPORT,MainWindow::fromDecToHex(Saturation, R, G, B));
+    WinExec(cmd, SW_HIDE);
+}
+
 void MainWindow::on_emotionBtn_clicked()
 {
+    ui->emotionBtn->setEnabled(false);
+    GetLocalTime(&timeForEmo);
+    MainWindow::emotionButtonPressed = true;
     imwrite("img.jpg", MainWindow::img);
     double* emotions = MainWindow::emotions_Analyze("img.jpg");
-    if (!emotions) return; //NULL
+    if (!emotions){
+        ui->emotionBtn->setEnabled(true);
+        return; //NULL
+    }
     char res[400];
-    sprintf(res, "%f\n%f\n%f\n%f\n%f\n%f\n%f\n%f\n", emotions[0], emotions[1],emotions[2],emotions[3],emotions[4],emotions[5], emotions[6], emotions[7]);
-    ui->emoLabel->setText(res);
+    sprintf(res, "Anger: %f\nContempt: %f\nDisgust: %f\nFear: %f\nHappiness: %f\nNeutral: %f\nSadness: %f\nSurprise: %f\n", emotions[0], emotions[1],emotions[2],emotions[3],emotions[4],emotions[5], emotions[6], emotions[7]);
     //ui->emoLabel->setText(res);
-    /*QPixmap pixmap("mask1.png");
-    //ui->emoLabel->setPixmap(pixmap);
-    QBitmap mask = pixmap.createMaskFromColor(Qt::black, Qt::MaskOutColor);
-    ui->emoLabel->setMask(mask);
 
-    ui->emoLabel->setStyleSheet("QLabel {width: 200px; height: 100px;  background: red; -moz-border-radius: 100px / 50px; -webkit-border-radius: 100px / 50px; border-radius: 100px / 50px;}");*/
-
-    //QRegion maskedRegion(ui->emoLabel->geometry().x(),ui->emoLabel->geometry().y(),ui->emoLabel->geometry().width(),ui->emoLabel->geometry().height(),QRegion::Ellipse);
-    //ui->emoLabel->setMask(maskedRegion);
-
+    struct EMOTION emos[8]={
+        "Anger",emotions[0],ANGER,
+        "Contempt",emotions[1],CONTEMPT,
+        "Disgust",emotions[2],DISGUST,
+        "Fear",emotions[3],FEAR,
+        "Happiness",emotions[4],HAPPINESS,
+        "Neutral",emotions[5],NEUTRAL,
+        "Sadness",emotions[6],SADNESS,
+        "Surprise",emotions[7],SURPRISE};
+    std::qsort((void*)emos,8,sizeof(struct EMOTION),compareEmos);
+    char top[200];
+    sprintf(top,"Top 3 emotions:\n%9s : %2.1f%%\n%9s : %2.1f%%\n%9s : %2.1f%%\n",emos[0].type, emos[0].value*100, emos[1].type, emos[1].value*100, emos[2].type, emos[3].value*100);
+    ui->topEmoLabel->setText(top);
     int maxEmoIndex = 0;
+    if (strcmp(emos[0].type,"Neutral") == 0 && emos[0].value < NEUTRAL_CORRECTION_PERCENT)
+        maxEmoIndex = 1;
 
-    for (int i = 1; i<8; i++){
-        if (emotions[i]>emotions[maxEmoIndex]){
-            if (i == 5 && emotions[i] > 0.93) //correction for neutral
-                maxEmoIndex = i;
-        }
+    int R = 0,G = 0,B = 0;
+
+    switch (emos[maxEmoIndex].type_e){
+    case ANGER: //anger
+        //230,37,50 //near purple
+        R = 230;
+        G = 37;
+        B = 50;
+        break;
+    case CONTEMPT: //contempt
+        //210,105,30 chocolate
+        R = 210;
+        G = 105;
+        B = 30;
+        break;
+    case DISGUST: //disgust
+        //145,102,173 //violet
+        R = 145;
+        G = 102;
+        B = 173;
+        break;
+    case FEAR: //fear
+        //4,139,70 green
+        R = 4;
+        G = 139;
+        B = 70;
+        break;
+    case HAPPINESS: //happiness
+        //255,170,0 orangered
+        R = 255;
+        G = 170;
+        B = 0;
+        break;
+    case NEUTRAL: //neutral
+        //206,223,233 gray with blue
+        R = 206;
+        G = 223;
+        B = 233;
+        break;
+    case SADNESS: //sadness
+        //37,128,186 deep blue
+        R = 37;
+        G = 128;
+        B = 186;
+        break;
+    case SURPRISE: //surprise
+       //241,205,21 yellow
+        R = 241;
+        G = 205;
+        B = 21;
+        break;
     }
-
-    char bgColor[10];
-    switch (maxEmoIndex){
-    case 0: //anger
-        strcpy(bgColor,"#FF0000"); //red
-        break;
-    case 1: //contempt
-        strcpy(bgColor,"#800020"); //burgundy
-        break;
-    case 2: //disgust
-        strcpy(bgColor,"#00FF00"); //green
-        break;
-    case 3: //fear
-        strcpy(bgColor,"#9F00C5"); //purple
-        break;
-    case 4: //happiness
-        strcpy(bgColor,"#FFFF00"); //yellow
-        break;
-    case 5: //neutral
-        strcpy(bgColor,"#FF7F00"); //orange
-        break;
-    case 6: //sadness
-        strcpy(bgColor,"#0000FF"); //blue
-        break;
-    case 7: //surprise
-        strcpy(bgColor,"#FFC0CB"); //pink
-        break;
-
+    if (currentEmotion != emos[maxEmoIndex].type_e){
+        music_PlayByEmo(emos[maxEmoIndex].type_e);
     }
+    currentEmotion = emos[maxEmoIndex].type_e;
+    MainWindow::sendColor(0,R,G,B);
     char cssForLabel[100];
-    sprintf(cssForLabel,"QLabel {border-radius: %dpx; background-color : %s;}", min(ui->emoLabel->geometry().width(),ui->emoLabel->geometry().height())/2, bgColor);
+    sprintf(cssForLabel,"QLabel {border-radius: %dpx; background-color : rgba(%d,%d,%d,255);}", min(ui->emoLabel->geometry().width(),ui->emoLabel->geometry().height())/2, R,G,B);
     ui->emoLabel->setStyleSheet(cssForLabel);
 
     delete[] emotions;
+    //ui->emotionBtn->setEnabled(true);
+}
+
+void MainWindow::music_PlayByEmo(enum emoEnum emo){
+
+    std::string str1;
+    //str1.toStdString();
+    QString trackName;
+    int i = rand() % 3 + 1;
+    switch (emo) {
+        case ANGER:
+                switch (i) {
+                case 1:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\angry\\disturbed_-_down_with_the_sickness.mp3";
+                    trackName = "Disturbed - Down with the sickness";
+                    break;
+                case 2:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\angry\\rammstein_-_mein_herz_brennt.mp3";
+                    trackName = "Rammstein - Mein Herz Brennt";
+                    break;
+                case 3:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\angry\\three_days_grace_-_animal_i_have_become.mp3";
+                    trackName = "Three Days Grace - Animal I have_become";
+                    break;
+                }
+                break;
+        case CONTEMPT:
+                switch (i) {
+                case 1:
+                    str1 = "";
+                    trackName = "Not yet added";
+                    break;
+                case 2:
+                    str1 = "";
+                    trackName = "Not yet added";
+                    break;
+                case 3:
+                    str1 = "";
+                    trackName = "Not yet added";
+                    break;
+                }
+                break;
+        case DISGUST:
+                switch (i) {
+                case 1:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\disgust\\ennio_morricone_-_inferno_bianco_synth.mp3";
+                    trackName = "Ennio Morricone - Inferno Bianco Synth";
+                    break;
+                case 2:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\disgust\\";
+                    trackName = "Not yet added";
+                    break;
+                case 3:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\disgust\\";
+                    trackName = "Not yet added";
+                    break;
+                }
+                break;
+        case FEAR:
+                switch (i) {
+                case 1:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\fear\\ennio_morricone_-_l'ultima_diligenza_di_red_rock.mp3";
+                    trackName = "Ennio Morricone - L'ultima diligenza";
+                    break;
+                case 2:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\fear\\ennio_morricone_-_overture.mp3";
+                    trackName = "Ennio Morricone - Overture";
+                    break;
+                case 3:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\fear\\marco_margna_-_stormy_ocean.mp3";
+                    trackName = "Ennio Morricone - Stormy Ocean";
+                    break;
+                }
+                break;
+        case HAPPINESS:
+                switch (i) {
+                case 1:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\happy\\mika_-_lollipop.mp3";
+                    trackName = "Mika - Lollipop";
+                    break;
+                case 2:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\happy\\pitbull_-_suavemente.mp3";
+                    trackName = "Pitbull - Suavemente";
+                    break;
+                case 3:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\happy\\smash_mouth_-_ all_star.mp3";
+                    trackName = "Smash Mouth - All Star";
+                    break;
+                }
+                break;
+        case NEUTRAL:
+                switch (i) {
+                case 1:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\neutral\\lewis_del_mar_-_loud(y).mp3";
+                    trackName = "Lewis Del Mar - Loud";
+                    break;
+                case 2:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\neutral\\the_police_-_every_breath_you_take.mp3";
+                    trackName = "The Police - Every Breath You Take";
+                    break;
+                case 3:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\neutral\\welshly_arms_-_hold_on_i'm_comin.mp3";
+                    trackName = "Welshly Arms - Hold On I'm comin'";
+                    break;
+                }
+                break;
+        case SADNESS:
+                switch (i) {
+                case 1:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\sad\\frank_sinatra_-_killing_me_softly_with_her_song.mp3";
+                    trackName = "Frank Sinatra - Killing me softly with her song";
+                    break;
+                case 2:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\sad\\sia_-_i_m_in_here.mp3";
+                    trackName = "Sia - I'm in here";
+                    break;
+                case 3:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\sad\\sting_-_shape_of_my_heart.mp3";
+                    trackName = "Sting - Shape of my heart";
+                    break;
+                }
+                break;
+        case SURPRISE:
+                switch (i) {
+                case 1:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\surprise\\aerosmith_-_the_other_side.mp3";
+                    trackName = "Aerosmith - The Other Side";
+                    break;
+                case 2:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\surprise\\rhcp_-_knock_me_down.mp3";
+                    trackName = "RHCP - Knock Me Down";
+                    break;
+                case 3:
+                    str1 = "D:\\QtProjects\\Opencv_Test2\\musicForProject\\surprise\\the_animals_-_boom_boom.mp3";
+                    trackName = "The Animals - Boom Boom";
+                    break;
+                }
+                break;
+    }
+    ui->musicTrackNameLabel->setText(trackName);
+    QString str2 = QString::fromStdString(str1);
+    player->setMedia(QUrl::fromLocalFile(QDir::toNativeSeparators(str2)));
+    player->setVolume(50);
+    qint64 playPosition = rand () % 15 + 20;
+    playPosition *= 1000;
+    player->play();
+    player->setPosition(player->position() + playPosition);
+
+
+}
+
+void MainWindow::on_musicPlayBtn_clicked()
+{
+    player->play();
+}
+
+void MainWindow::on_musicPauseBtn_clicked()
+{
+    player->pause();
+}
+
+void MainWindow::on_musicMuteBtn_clicked()
+{
+    if (player->volume() == 50){
+        player->setVolume(0);
+    }
+    else player->setVolume(50);
 }
